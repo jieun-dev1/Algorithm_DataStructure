410972ccbf65f05fd4d96414e04a74b055537d58
jieun-dev1 <jkdev1324@gmail.com>
2022-06-04 23:43:36 +0900

    구간합/최소값

--- src/com/company/SegmentTree/Prefix_min.java
+++ src/com/company/SegmentTree/Prefix_min.java
@@ -19,10 +19,10 @@ public class Prefix_min {
 
     static int query(int[] tree, int node, int start, int end, int left, int right) {
         if (left > end || right < start) {
-            return -1; // 합을 구할 때는 0을 넣었었다.
+            return -1; // 합을 구할 때는 0을 넣었었다. index 범위 밗일 때는 최소값이라는 게 없으니까 -1이 됨.
         }
         if (left <= start && end <= right){
-            return tree[node];
+            return tree[node]; //ex) node 가 1일 때 tree 배열의 1인덱스를 리턴하는 것.
         }
 
         int lmin = query (tree, node*2, start, (start+end)/2, left, right);
@@ -52,7 +52,7 @@ public class Prefix_min {
 
         update(a, tree, node*2, start, (start+end)/2, index, val);
         update(a, tree, node*2+1, (start+end)/2+1, end, index, val);
-        tree[node] = Math.min(tree[node*2], tree[node*2+1]);
+        tree[node] = Math.min(tree[node*2], tree[node*2+1]); //update 된 값으로, min 값을 업데이트 해야하기 때문. 상위 레벨에서 min 값이 바뀔 수도 있으니깐.
     }
 
     //Integer.ParseInt. String 타입 숫자를 int 로 변환.
@@ -74,16 +74,16 @@ public class Prefix_min {
         int m = Integer.parseInt(br.readLine());
         //--는 m--; m>0; 두 가지 조건을 합친 것이다. 이럴 경우 m 번만큼 반복이 가능함. 6,5,4,3,2,1 만큼 while 문을 도는 것.
         while(m--> 0) {
-            line = br.readLine().split(" ");
-            int what = Integer.parseInt(line[0]);
+            line = br.readLine().split(" "); //[2,1,3]
+            int what = Integer.parseInt(line[0]); //what은 1 혹은 2. 첫 번째 값 읽기.
             if(what == 1) {
-                int index = Integer.parseInt(line[1]);
-                int val = Integer.parseInt(line[2]);
-                update(a, tree, 1, 0, n-1, index-1, val);
-            } else {
+                int index = Integer.parseInt(line[1]); //1이면 첫 번쨰 인덱스
+                int val = Integer.parseInt(line[2]);  //값을 바꿔준다.
+                update(a, tree, 1, 0, n-1, index-1, val); //index 가 0부터 시작하기 때문이다. 인덱스는 n-1까지가 맞다.
+            } else { // 2인 경우.
                 int left = Integer.parseInt(line[1]);
                 int right = Integer.parseInt(line[2]);
-                bw.write(query(tree, 1, 0, n-1, left-1, right-1) + "\n");
+                bw.write(query(tree, 1, 0, n-1, left-1, right-1) + "\n"); //얘가 곧 tree 의 시작점인 tree[1]이다.
             }
         }
         bw.flush();
